cd17043e8bdcb3898b95c68bd71c531e13c4dc0f
Santos <santos@gmail.com>
2020-03-26 07:55:28 -0300

    Changes in templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.SignUp.as_view(),name="signup"),
+    path('register/', views.SignUp.as_view(), name="signup"),
 ]
-
-# 
-# 
+ 
